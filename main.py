52c8ab924f5a00a590c1808b2ada441a7f1ec37e
Sevda Avcılar <sevdavcilar@gmail.com>
2021-10-12 13:48:04 +0300

    degerlerisifirla fonk eklendi

--- main.py
+++ main.py
@@ -11,16 +11,26 @@ class MainWindow(QMainWindow):
         loadUi("untitled.ui",self)
         self.tablo.setColumnWidth(6,129) #6.sütunun genişliğini 129px'e sabitledik.
         self.hesapla.clicked.connect(self.loaddata)
+        self.sonsifirla.clicked.connect(self.tablo.clearContents)
+        self.degsifirla.clicked.connect(self.degerlerisifirla)
         print(fc.MilkObj.name)
+        self.area=["bread1","bread2"]
+
     def loaddata(self):
-        foods=[]
+        foods=[fc.MilkObj,fc.FatObj]
         row=0
         self.tablo.setRowCount(len(foods))
         for food in foods: #Sonuçları satırlar halinde tabloda gösteren döngü
-            self.tablo.setItem(row,0,QtWidgets.QTableWidgetItem(food["sut"]))
-            self.tablo.setItem(row, 1, QtWidgets.QTableWidgetItem(food["et"]))
-            self.tablo.setItem(row, 2, QtWidgets.QTableWidgetItem(food["ekmek"]))
+            self.tablo.setItem(row,0,QtWidgets.QTableWidgetItem(str(food.change)))
+            self.tablo.setItem(row, 1, QtWidgets.QTableWidgetItem(str(food.fat)))
             row=row+1
+
+    """def degerlerisifirla(self): //çözülmedi
+        for item in self.area:
+            print(item)
+            self.item.clear"""
+
+
 
 if __name__ == "__main__":
     app=QApplication(sys.argv)
